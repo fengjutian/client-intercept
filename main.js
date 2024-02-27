fcbbda773e92e10880304fd4d1dca00a1ae02188
fengjutian <jutian.feng@epro.com.cn>
2024-02-27 11:06:36 +0800

    新加express 服务

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-// import { navigatorSendBeacon } from './beacon.js';
-// import { LogServiceURL } from './config.js';
+var express = require('express');
+var app = express();
+const path = require('path');
+app.use('/static',express.static(path.join(__dirname,'public')));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+});// 显示html页面
 
-// export default {
-//     navigatorSendBeacon,
-//     LogServiceURL
-// }
+var server = app.listen(8080, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+  //  服务器IP地址为127.0.0.1 端口为8888
+   console.log( "server is running"+host+port);
+ });
+
